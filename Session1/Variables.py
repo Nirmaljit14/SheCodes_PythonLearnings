399d43c6c792d74c77ddfc994a868ef0758993e1
Nirmaljit Kaur <Nirmaljit.Kaur@riotinto.com>
2023-06-06 11:50:21 +1000

    Session 1 HomeWork

---
+++ Session1/Variables.py
@@ -0,0 +1,14 @@
+
+#Program that takes the user's name and height (in centimeters) and output
+# a summary sentence 
+
+username = str(input("What is your name?"))
+userHeight = int(input("How tall are you?"))
+height_cms = 0.328 * userHeight
+
+print (f" The {username} is {userHeight} cms tall")
+
+
+
+
+
