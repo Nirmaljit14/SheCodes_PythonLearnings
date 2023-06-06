3f20f10d10ca26d6eb5bbf6b717a19bf990d0209
Nirmaljit Kaur <Nirmaljit.Kaur@riotinto.com>
2023-06-06 12:37:27 +1000

    Updated methods

--- Session1/Variables.py
+++ Session1/Variables.py
@@ -1,12 +1,32 @@
+# How to get Data from user 
+print("Hello")
+num1   = input("Please add a number 1 ")
+num2 = input ("Please add a number 2 ")
+print (f"The sum of num1 and num2 is = {int(num1) + float(num2)}")
+
+
+num1   = input("Please add a number 1 ")
+num2 = input ("Please add a number 2 ")
+print (f"The sum of num1 and num2 is = {float(num1) - int(num2)}")
+
+
+num1   = input("Please add a number 1 ")
+num2 = input ("Please add a number 2 ")
+print (f"The sum of num1 and num2 is = {float(num1) + float(num2)}")
+
+num1   = input("Please add a number 1 ")
+num2 = input ("Please add a number 2 ")
+print (f"The sum of num1 and num2 is = {float(num1) * float(num2)}")
 
 #Program that takes the user's name and height (in centimeters) and output
 # a summary sentence 
 
 username = str(input("What is your name?"))
-userHeight = int(input("How tall are you?"))
-height_cms = 0.328 * userHeight
+userHeight = float(input("How tall are you?"))
+height_cms = userHeight * 30.48
 
-print (f" The {username} is {userHeight} cms tall")
+print (f" The {username} is {height_cms}cms tall")
+
 
 
 
