63c649be19e40b2800af98be94fef672d732878b
nirmaljitkaur <nirmalkahlon8@gmail.com>
2023-06-07 16:20:35 +1000

    Homeswork_until_WHILE Loop

--- Session3/Loops.py
+++ Session3/Loops.py
@@ -30,12 +30,62 @@
 # for number in range(10):
 #     print(number)
 
-students = [
-    ["Cindy","Emily","Eve"],
-    ["Julie","Yara","Sargun"],
-    ["Jenny","Sarah","Maddy"]
-    ]
-for student in students:
-    print(student)
-    for name in student:
-        print(name)
+# students = [
+#     ["Cindy","Emily","Eve"],
+#     ["Julie","Yara","Sargun"],
+#     ["Jenny","Sarah","Maddy"]
+#     ]
+# for student in students:
+#     print(student)
+#     for name in student:
+#         print(name)
+
+# count = 0
+# while 5 > count:
+#     print(f"Hello, the number is {count}")
+#     count += 1
+
+
+                            # Exercise #
+# Q1 Continuously ask the user to enter a number until they provide a blank input. Outputthe sum of all the numbers.
+
+count = 0
+num1 = input("Please enter a number")
+while num1 != '':
+   count = count + int(num1)
+   num1 = input("Please enter a number")
+print(count)
+
+# Q2
+# Ask the user to enter a in integer number. Print all the odd numbers between 0 andthat number (inclusive).
+
+number = int(input("Please enter a number"))
+
+digit = 0
+while digit <= number:
+   if digit % 2 != 0:
+      print(digit)
+   digit += 1
+
+# Q3 Write a guessing game.
+# Select a number, and save it as a variable in your code. 
+# Ask the user to enter anumber, and then output whether their number is less than or greater than theselected number. 
+# Keep asking until the user guesses the correct number. Then print acongratulatatory message.
+
+
+print("Welcome to the Guessing Game")
+
+number = int(input("Please enter a number "))
+
+lucky_number = 7
+
+while number >= 0:
+   if number < 100 and number != lucky_number:
+      print("Oh Ho! you missed it, try again")
+      number = int(input("Please enter the number again "))
+   if number == lucky_number:
+      print("You Nailed it ")
+      break
+
+
+
