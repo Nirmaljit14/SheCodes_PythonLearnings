cb34e3ab42418791d2552e8f804125e30037b3cd
nirmaljitkaur <nirmalkahlon8@gmail.com>
2023-06-10 10:15:45 +1000

    Functions learned

---
+++ Session4/Functions.py
@@ -0,0 +1,31 @@
+# Function - An activity that is natural to or the purpose of a person or thing
+# Functions we've already have are -
+# input(), len(), int(), print()
+
+# Task Separation
+
+# def ask_userName():
+#     name = input("What is your name? ")
+#     return name
+
+# print("Hello")
+# answer = ask_userName()
+# print(answer)
+
+# def ask_userAge():
+#     age = int(input("Please enter your age?"))
+#     if age >= 18:
+#         print("Welcome")
+#     else:
+#         print("You are underage")
+
+# Funciton definition
+
+
+# Parameters
+def add (num1, num2):
+    result = num1 + num2
+    return result
+
+answer = add(2,3)
+print(answer)
